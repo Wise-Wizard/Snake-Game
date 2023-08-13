3ec451a808f51dfa1d1059cd3d1bd37e177ec1ba
Wise-Wizard <saransh.shankar@gmail.com>
2023-08-13 14:38:00 +0530

    Created Food Class

---
+++ food.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.speed("fastest")
+        self.shape("circle")
+        self.color("#865DFF")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        x_value = random.randint(-280, 280)
+        y_value = random.randint(-280, 280)
+        self.goto(x_value, y_value)
+    def new_position(self):
+        new_x_value = random.randint(-280, 280)
+        new_y_value = random.randint(-280, 280)
+        self.goto(new_x_value, new_y_value)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 import random
 from snake import Snake
+from food import Food
 import time
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,6 +9,7 @@ screen.title("My Snake Game")
 screen.bgcolor("#27374D")
 screen.listen()
 snake = Snake()
+food = Food()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -17,4 +19,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.05)
     snake.move()
+    
+    #Detect Collision
+    if snake.head.distance(food) < 15:
+        food.new_position()
 screen.exitonclick()
